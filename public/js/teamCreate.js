c7dfc986bf1e5e53d589cf94a3f422a697b95274
Kahlil Goldstein <kahlilgoldstein@gmail.com>
2020-10-01 13:26:03 -0500

    fixed indentation errors

--- public/js/teamCreate.js
+++ public/js/teamCreate.js
@@ -1,39 +1,39 @@
 $(document).ready(() => {
-    // Getting references to our form and input
-    const teamCreateForm = $("form.teamCreate");
-    const nameInput = $("input#teamName-input");
+  // Getting references to our form and input
+  const teamCreateForm = $("form.teamCreate");
+  const nameInput = $("input#teamName-input");
 
   
-    // When the signup button is clicked, we validate the email and password are not blank
-    teamCreateForm.on("submit", event => {
-      event.preventDefault();
-      const userData = {
-        name: nameInput.val().trim(),
-      };
+  // When the signup button is clicked, we validate the email and password are not blank
+  teamCreateForm.on("submit", event => {
+    event.preventDefault();
+    const userData = {
+      name: nameInput.val().trim(),
+    };
   
-      if (!userData.name) {
-        return;
-      }
-      // If we have an email and password, run the signUpUser function
-      teamCreate(userData);
-      nameInput.val("");
-    });
+    if (!userData.name) {
+      return;
+    }
+    // If we have an email and password, run the signUpUser function
+    teamCreate(userData);
+    nameInput.val("");
+  });
   
-    // Does a post to the signup route. If successful, we are redirected to the members page
-    // Otherwise we log any errors
-    function teamCreate(userData) {
-      $.post("/api/signup", {
-        team_name: userData.name,
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function teamCreate(userData) {
+    $.post("/api/signup", {
+      team_name: userData.name,
+    })
+      .then(() => {
+        window.location.replace("/team");
+        // If there's an error, handle it by throwing up a bootstrap alert
       })
-        .then(() => {
-          window.location.replace("/team");
-          // If there's an error, handle it by throwing up a bootstrap alert
-        })
-        .catch(handleLoginErr);
-    }
+      .catch(handleLoginErr);
+  }
   
-    function handleLoginErr(err) {
-      $("#alert .msg").text(err.responseJSON);
-      $("#alert").fadeIn(500);
-    }
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
 });
