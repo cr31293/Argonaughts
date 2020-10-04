b2ce159826d5ef15fb813d228f7a197b4758f96f
Chris Reed <cr31293@gmail.com>
2020-10-04 17:11:03 -0400

    adding home.html href and team info to member js

--- public/js/members.js
+++ public/js/members.js
@@ -7,4 +7,21 @@ $(document).ready(() => {
     $(".member-info").append(`<li>${data.teamId}</li>`);
     $(".member-info").append(`<li>${data.mercenaryStatus}</li>`);
   });
+
+  $.get("/api/user_data").then(data => {
+  if (data.teamId) {
+    $.get("/api/team_data").then(data => {
+      $(".team-name").text(data.teamName);
+      $(".team-info").append(`<li>${data.teamRank}</li>`);
+      $(".team-info").append(`<li>${data.wins}</li>`);
+      $(".team-info").append(`<li>${data.losses}</li>`);
+      $(".team-info").append(`<li>${data.winRate}</li>`);
+    })
+  } else {
+    $(".team-info").append(`To view your team information please create or join a team!`);
+    $(".team-info").append(`<a class="btn btn-primary btn-lg m-2" href="#" role="button">Create A Team</a>`);
+    $(".team-info").append(`<a class="btn btn-primary btn-lg m-2" href="#" role="button">Join A Team</a>`);
+
+  }
+});
 });
